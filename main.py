9500250a1fb11fc279cf2c11fa6d1681559e907f
emanuele <emanuele.studio01@gmail.com>
2022-10-27 11:40:16 +0200

    secondo backup

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# This is a sample Python script.
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+from TrattamentiConsulenze.ViewsTrattCons.GestioneConsView import GestioneConsView
+
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,9 +13,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = GestioneConsView()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
