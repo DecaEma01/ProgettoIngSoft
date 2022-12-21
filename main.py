2a46d67a9cf0b69fd2726182fc4ffee98f4892f0
emanuele <emanuele.studio01@gmail.com>
2022-12-21 10:10:57 +0100

    42o backup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sched
 
-import schedule
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
@@ -8,34 +7,15 @@ import os
 from datetime import datetime
 import threading
 
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from Dipendenti.ModelsDipendenti.SegretarioModel import SegretarioModel
 from Generale.GeneraleViews.LoginView import LoginView
-from Pazienti.ViewsPazienti.GestionePazientiView import GestionePazientiView
-from Prenotazioni.ViewsPrenotazioni.GestionePrenotazioniView import GestionePrenotazioniView
-from TrattamentiConsulenze.ViewsTrattCons.AggiungiTrattView import AggiungiTrattView
-from TrattamentiConsulenze.ViewsTrattCons.GestioneConsView import GestioneConsView
-from TrattamentiConsulenze.ControllersTrattCons.ConsulenzaController import ConsulenzaController
-from TrattamentiConsulenze.ViewsTrattCons.GestioneTrattConsView import GestioneTrattConsView
-from Pazienti.ViewsPazienti.GestionePazientiView import GestionePazientiView
-
-
-from Pazienti.ModelsPazienti.PazienteModel import PazienteModel
-from Pazienti.ModelsPazienti.ElencoPazientiModel import ElencoPazientiModel
 from Generale.Backup.BackupModel import BackupModel
 
 import time
 
-def print_hi():
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
-
 def backupGiornaliero():
 
     dataOggi=datetime.today().strftime('%Y-%m-%d')
-    orarioBackup = " 00:16:00"
+    orarioBackup = " 22:00:00"
     dataOrarioBackup = str(dataOggi+orarioBackup)
     datatimeBackup = datetime.strptime(dataOrarioBackup, '%Y-%m-%d %H:%M:%S')
 
@@ -55,11 +35,6 @@ def backupGiornaliero():
 
     print("fatto backup")
 
-#def backupAlternativo():
-#    schedule.every(5).seconds.do(print_hi)
-#    while True:
-#        schedule.run_pending()
-#       #backupAlternativo()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -73,71 +48,4 @@ if __name__ == '__main__':
     sys.exit(app.exec())
 
 
-
-
-
-"""
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = GestionePrenotazioniView()
-    ui.setupUi(Form, app)
-    Form.show()
-    sys.exit(app.exec_())
-"""
-
-"""
-    if os.path.isfile('Dati/Pazienti.pickle') == False:
-        paz = PazienteModel('Nicholas', 'Bradach', 'BRDNHL', '345', 'SN', '26', 'MSV', 'Ancona', {})
-    pazienti = ElencoPazientiModel.visualizzaElencoPazientiM(PazienteModel)
-
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = GestionePazientiView()
-    ui.setupUi(Form, app)
-    Form.show()
-    sys.exit(app.exec_())
-"""
-
-"""
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    view = GestionePazientiView()
-    view.show()
-    sys.exit(app.exec())
-"""
-"""
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = GestioneTrattConsView()
-    ui.setupUi(Form,app)
-    Form.show()
-    sys.exit(app.exec_())
-"""
-
-"""
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = AggiungiTrattView()
-    ui.setupUi(Form,app)
-    Form.show()
-    sys.exit(app.exec_())
-"""
-
-""" 
-test gestisci consulenza
-
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = GestioneConsView()
-    ui.setupUi(Form,app)
-    Form.show()
-    sys.exit(app.exec_())
-"""
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
